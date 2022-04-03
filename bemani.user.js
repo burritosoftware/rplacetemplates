97456ca1fd5c93e3f7493673aca125b67c71d05c
Goatgarien <otaku4532@gmail.com>
2022-04-02 23:29:53 -0700

    description

--- bemani.user.js
+++ bemani.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Arcaea Logo template
+// @name         Bemani Logo template
 // @namespace    http://tampermonkey.net/
 // @version      0.3
 // @description  try to take over the canvas!
-// @author       oralekin, LittleEndu, ekgame
+// @author       oralekin, LittleEndu, ekgame, Goatgarien
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
