f339da196e68f019d9909069e4fafbb0284a23ff
Goatgarien <otaku4532@gmail.com>
2022-04-02 23:25:01 -0700

    bemani

--- arcaeasdvx.user.js
+++ arcaeasdvx.user.js
@@ -12,7 +12,7 @@ if (window.top !== window.self) {
     window.addEventListener('load', () => {
         // Load the image
         const image = document.createElement("img");
-        image.src = "https://github.com/burritosoftware/rplacetemplates/raw/master/arcaeatemplate.png";
+        image.src = "https://github.com/Goatgarien/rplacetemplates/raw/master/BEMANI.png";
         image.onload = () => {
             image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
         };
